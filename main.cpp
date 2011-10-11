4939ad0ec2a0bd24a2d39cf581cbdf15cb12a063
milanotiffany <milant@rpi.edu>
2011-10-11 01:47:02 -0300

    Edited main.cpp via GitHub

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
-//Operating System FALL2011
-//Project1
-//Tiffany Milano & Meng Jung Lin
-//main.cpp
+// Tiffany Milano and Meng Jung Lin 
+// Project 1 - Operating Systems 
+// October 20th, 2011
+
+
+#include <iostream> 
+#include Sim.h
+
 
 int main()
 {
