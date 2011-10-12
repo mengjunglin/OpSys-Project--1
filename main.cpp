516d39d31f3d6529c6d07aedc8ff7a7f1e36b836
Candy Lin <candy800419@yahoo.com.tw>
2011-10-11 20:57:47 -0400

    testing

--- main.cpp
+++ main.cpp
@@ -2,7 +2,14 @@
 // Project 1 - Operating Systems 
 // October 20th 2011
 
+#include <iostream>
+#include <classSim.h>
+using namespace std;
+
 int main()
 {
-  cout << "Hello World";
+	cout << "Hello World" < endl;
+
+	system("pause");
+	return 0;
 }
