96fce6429ef35c3b0de5c11beb80d0bb87836103
Candy Lin <candy800419@yahoo.com.tw>
2011-10-12 12:38:06 -0400

    yes?no?

--- main.cpp
+++ main.cpp
@@ -3,12 +3,18 @@
 // October 20th 2011
 
 #include <iostream>
-#include <classSim.h>
+#include "classSim.h"
 using namespace std;
 
 int main()
 {
-	cout << "Hello World" < endl;
+	//just testing if im doing this right...
+	cout << "Hello World" << endl;
+
+	Sim simulation;
+	simulation.setiTime(20);
+	int k = simulation.getITime();
+	cout << k << endl;
 
 	system("pause");
 	return 0;
