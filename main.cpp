329c5b2d7a238e6fb96eedc6d2f6aaa83285054e
Candy Lin <candy800419@yahoo.com.tw>
2011-10-13 22:54:30 -0400

    Added dataToCollect function

--- main.cpp
+++ main.cpp
@@ -14,13 +14,16 @@ void psjf(Sim* p);  // Preemptive Shortest-Job First (SJF)
 void rr(Sim* p);    // Round-Robin (RR)
 void pp(Sim* p);    // Preemptive Priority (PP) 
 
+void dataToCollect(Sim* p, int minTurnAround, int maxTurnAround, int turnTotal, int minInitialWait, 
+	int maxInitialWait, int initialTotal, int minWaitTime, int maxWaitTime, int totalWait);
+
 // Simulate elapsed time using a global variable 
 int elapsedTime = 0; 
 
 
 int main(int argc, char * argv[])
 {	
-	if(argc > 3) { 
+	if(argc > 2) { 
 		cout << "Error: too many arguments, enter either 1 or 2 arguments."; 
 		return 1; 
 	}
@@ -57,7 +60,54 @@ int main(int argc, char * argv[])
 	return 0;
 }
 
+void dataToCollect(Sim* p, int minTurnAround, int maxTurnAround, int turnTotal, int minInitialWait, 
+	int maxInitialWait, int initialTotal, int minWaitTime, int maxWaitTime, int totalWait){
+	
+	for (int i = 0; i < 20; i++)
+	{
+		//TurnAroundTime
+		if(p[i].getTurnTime() < minTurnAround){
+			minTurnAround = p[i].getTurnTime();
+		}
+		if(p[i].getTurnTime() > maxTurnAround){
+			maxTurnAround = p[i].getTurnTime();
+		}
+		turnTotal += p[i].getTurnTime(); 
+			
+		//InitialWaitTime
+		if(p[i].getITime() < minInitialWait){
+			minInitialWait = p[i].getITime();
+		}
+		if(p[i].getITime() > maxInitialWait){
+			maxInitialWait = p[i].getITime();
+		}
+		initialTotal += p[i].getITime(); 
+			
+		//TotalWaitTime
+		if(p[i].getWaitTime() < minWaitTime){
+			minWaitTime = p[i].getWaitTime();
+		}
+		if(p[i].getWaitTime() > maxWaitTime){
+			maxWaitTime = p[i].getWaitTime();
+		}
+		totalWait += p[i].getWaitTime();
+	}
+	
+	/* int minTurnAround, int maxTurnAround, int turnTotal, int minInitialWait, 
+	int maxInitialWait, int initialTotal, int minWaitTime, int maxWaitTime, int totalWait */ 
+	cout << "\n\n\n"; 
+	cout << "Minimum Turn Around Time: " << minTurnAround << "\n"; 
+	cout << "Maximum Turn Around Time: " << maxTurnAround << "\n"; 
+	cout << "Average Turn Around Time: " << turnTotal/20 << "\n\n";
 
+	cout << "Minimum Initial Wait Time: " << minInitialWait << "\n"; 
+	cout << "Maximum Initial Wait Time: " << maxInitialWait << "\n"; 
+	cout << "Average Initial Wait Time: " << initialTotal/20 << "\n\n";
+
+	cout << "Minimum Wait Time: " << minWaitTime << "\n"; 
+	cout << "Maximum Wait Time: " << maxWaitTime << "\n"; 
+	cout << "Average Wait Time: " << totalWait/20 << "\n\n";
+}
 
 
 /* First-Come First Served(FCFS)
@@ -66,8 +116,9 @@ int main(int argc, char * argv[])
  * process in is the first process to run and terminate */ 
 void fcfs(Sim* p)
 {
-	int tempWait = 0;
-	int eTemp;
+	int tempWait = 0, eTemp, minTurn = p[0].getTurnTime(), maxTurn = 0, turnT = 0, minInitial = p[0].getITime(), 
+		maxInitial = 0, initialT = 0, minWait = p[0].getWaitTime(), maxWait = 0, totalW = 0;  
+
 	// elapsedTime = 0; // Clear out the Global Variable "elapsedTime" 
 
 	/* This for loop sets the inital times for each process and 
@@ -75,6 +126,7 @@ void fcfs(Sim* p)
 	cout << "Create Processes: \n"; 
 	for (int i = 0; i < 20; i++)
 	{
+
 		if (i != 0)
 		{
 			tempWait += p[i-1].getcTime();
@@ -98,10 +150,11 @@ void fcfs(Sim* p)
 		}
 		cout << "[time " << p[j].getWaitTime() << "ms] Process " << j+1 << " access CPU for the first time (wait time " << p[j].getITime() << "ms)\n";	
 		cout << "[time " << p[j].getTerminateTime() << "ms] Process " << j+1 << " terminated (turnaround time " << p[j].getTurnTime() << "ms, wait time " << p[j].getWaitTime() << "ms)\n"; 
-	}
+	} 
 
-	cout << "\n\n\n"; 
+	dataToCollect(p, minTurn, maxTurn, turnT, minInitial, maxInitial, initialT, minWait, maxWait, totalW);
 }
+
 
 /* Shortest-Job First (SFS)
  * This will sort the processes that are sent in based on the 
